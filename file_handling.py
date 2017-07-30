7b43efd99c1e8e34f73486f02a1aa77f5bf63034
Amol Kahat <akahat@redhat.com>
2017-07-30 13:04:25 +0530

    Simpel file operations

--- file_handling.py
+++ file_handling.py
@@ -1,8 +1,9 @@
 file_name = "my_tasks.txt"
 
-with open(file_name, 'w') as f:
-    f.write("1. Attend the meetup")
-    f.write("2. Go home and get sleep")
-    f.write("3. Make sure you had lunch before going to bed.")
+f = open(file_name, 'w')
+f.write("1. Attend the meetup")
+f.write("2. Go home and get sleep")
+f.write("3. Make sure you had lunch before going to bed.")
+f.close()
 
 print("File written successfully")
