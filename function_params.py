70898c9162a56180ffed1d9873fbb98d76e18d25
Amol Kahat <akahat@redhat.com>
2017-07-30 11:23:30 +0530

    Function params ** kwargs

--- function_params.py
+++ function_params.py
@@ -1,6 +1,7 @@
 
-def print_msg(msg, error="No Error"):
+def print_msg(msg, error="No Error", *kwargs):
     print("Log : "+error+":" + msg)
+    print(kwargs)
 
 
-print_msg("This will be logged", "File not found")
+print_msg("This will be logged","File not found", "1", "12", "213", "32113")
